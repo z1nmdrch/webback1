e4ba6af5344d72e4765f3d6485f1b55955f030f3
Zangar <zangar.zhunisbekov@gmail.com>
2024-01-15 23:58:34 +0600

    3

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,46 @@
-function validateForm() {
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
-  
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-      alert('Please enter valid weight and height.');
-      return false;
-    }
-  
-    return true;
-  }
-  
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('bmiForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(document.getElementById('bmiForm'));
+    const formDataObject = {};
+    
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
+
+    fetch('/bmicalculator', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formDataObject),
+    })
+    .then(response => response.json())
+    .then(data => {
+      const resultElement = document.getElementById('bmiResultText');
+      resultElement.textContent = `BMI Result: ${data.bmi}, Category: ${data.bmiCategory}`;
+      document.getElementById('bmiResultContainer').style.display = 'block';
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      const resultElement = document.getElementById('bmiResultText');
+      resultElement.textContent = 'Error calculating BMI. Please check your input.';
+      document.getElementById('bmiResultContainer').style.display = 'block';
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    fetch('/history')
+    .then(response => response.json())
+    .then(data => {
+      const bmiHistoryList = document.getElementById('bmiHistory');
+      data.forEach(record => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `BMI Result: ${record.bmi}, Category: ${record.bmiCategory}, Time: ${record.timestamp}`;
+        bmiHistoryList.appendChild(listItem);
+      });
+    })
+    .catch(error => console.error('Error:', error));
+  });
+});
--- root.js
+++ root.js
@@ -4,6 +4,7 @@ const bmiRoutes = require('./routes/bmiRoutes');
 
 const app = express();
 const port = 3000;
+app.use(express.json())
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
